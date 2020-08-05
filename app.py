7780d6697428e1076769d0ef6112420e66931bb0
financehacks <59261823+financehacks@users.noreply.github.com>
2020-08-04 21:13:10 -0700

    added code and datasets for candlestick screener with ta-lib

---
+++ app.py
@@ -0,0 +1,50 @@
+import os, csv
+import talib
+import yfinance as yf
+import pandas
+from flask import Flask, escape, request, render_template
+from patterns import candlestick_patterns
+
+app = Flask(__name__)
+
+@app.route('/snapshot')
+def snapshot():
+    with open('datasets/symbols.csv') as f:
+        symbols = f.read().splitlines()
+        for symbol in symbols:
+            data = yf.download(symbol, start="2020-01-01", end="2020-08-01")
+            data.to_csv('datasets/daily/{}.csv'.format(symbol))
+
+    return {
+        "code": "success"
+    }
+
+@app.route('/')
+def index():
+    pattern  = request.args.get('pattern', False)
+    stocks = {}
+
+    with open('datasets/symbols.csv') as f:
+        for row in csv.reader(f):
+            stocks[row[0]] = {'company': row[1]}
+
+    if pattern:
+        for filename in os.listdir('datasets/daily'):
+            df = pandas.read_csv('datasets/daily/{}'.format(filename))
+            pattern_function = getattr(talib, pattern)
+            symbol = filename.split('.')[0]
+
+            try:
+                results = pattern_function(df['Open'], df['High'], df['Low'], df['Close'])
+                last = results.tail(1).values[0]
+
+                if last > 0:
+                    stocks[symbol][pattern] = 'bullish'
+                elif last < 0:
+                    stocks[symbol][pattern] = 'bearish'
+                else:
+                    stocks[symbol][pattern] = None
+            except Exception as e:
+                print('failed on filename: ', filename)
+
+    return render_template('index.html', candlestick_patterns=candlestick_patterns, stocks=stocks, pattern=pattern)
---
+++ pattern_detect.py
@@ -0,0 +1,15 @@
+import talib
+import yfinance as yf
+
+data = yf.download("SPY", start="2020-01-01", end="2020-08-01")
+
+morning_star = talib.CDLMORNINGSTAR(data['Open'], data['High'], data['Low'], data['Close'])
+
+engulfing = talib.CDLENGULFING(data['Open'], data['High'], data['Low'], data['Close'])
+
+data['Morning Star'] = morning_star
+data['Engulfing'] = engulfing
+
+engulfing_days = data[data['Engulfing'] != 0]
+
+print(engulfing_days)
