2a6ffae5ea35f7d864f16f3870d65a35b7ad8b04
visweswaran1998 <visweswaran.nagasivam98@gmail.com>
2020-09-27 10:47:11 +0530

    :whale: :hammer: :sparkles: Finalized Docker Image

--- app.py
+++ app.py
@@ -9,13 +9,13 @@ app = Flask(__name__)
 
 @app.route('/snapshot')
 def snapshot():
-    with open(os.path.join(os.getcwd(), 'datasets/symbols.csv')) as f:
+    with open('datasets/symbols.csv') as f:
         for line in f:
             if "," not in line:
                 continue
             symbol = line.split(",")[0]
             data = yf.download(symbol, start="2020-01-01", end="2020-08-01")
-            data.to_csv(os.path.join(os.getcwd(), 'datasets/daily/{}.csv').format(symbol))
+            data.to_csv('datasets/daily/{}.csv'.format(symbol))
 
     return {
         "code": "success"
@@ -26,13 +26,13 @@ def index():
     pattern  = request.args.get('pattern', False)
     stocks = {}
 
-    with open(os.path.join(os.getcwd(), 'datasets/symbols.csv')) as f:
+    with open('datasets/symbols.csv') as f:
         for row in csv.reader(f):
             stocks[row[0]] = {'company': row[1]}
 
     if pattern:
-        for filename in os.listdir(os.path.join(os.getcwd(), 'datasets/daily')):
-            df = pandas.read_csv(os.path.join(os.getcwd(), 'datasets/daily/{}').format(filename))
+        for filename in os.listdir('datasets/daily'):
+            df = pandas.read_csv('datasets/daily/{}'.format(filename))
             pattern_function = getattr(talib, pattern)
             symbol = filename.split('.')[0]
 
